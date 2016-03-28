f94e956e819baab19878dbb8de57d19fefe9e64f
Kevin Roark <ker2143@columbia.edu>
2016-03-27 23:14:10 -0700

    animate some square drawing

--- js/main.js
+++ js/main.js
@@ -7,22 +7,26 @@
     var canvas = document.querySelector('.background-canvas');
     var context = canvas.getContext('2d');
 
+    var distanceBetweenVerticalLines = 40;
+    var distanceBetweenHorizontalLines = 40;
+    var squareAnimationInterval;
+
     redrawCanvas();
     window.addEventListener('resize', redrawCanvas, false);
 
     function redrawCanvas() {
       canvas.width = window.innerWidth;
       canvas.height = window.innerHeight;
+      context.clearRect(0, 0, canvas.width, canvas.height);
 
       drawGrid();
+      resetSquareAnimationInterval();
     }
 
     function drawGrid() {
       context.lineWidth = 2;
       context.strokeStyle = 'black';
 
-      var distanceBetweenVerticalLines = 40;
-      var distanceBetweenHorizontalLines = 40;
       var maxX = window.innerWidth + distanceBetweenVerticalLines * 2;
       var maxY = document.body.scrollHeight + distanceBetweenHorizontalLines * 2;
 
@@ -39,16 +43,78 @@
         context.lineTo(x, canvas.height);
         context.stroke();
       }
+    }
 
+    function resetSquareAnimationInterval() {
+      clearInterval(squareAnimationInterval);
+
+      var maxX = window.innerWidth + distanceBetweenVerticalLines * 2;
+      var maxY = document.body.scrollHeight + distanceBetweenHorizontalLines * 2;
       var numberToFill = Math.floor(Math.random() * 20) + 15;
       var fillChoices = ['black', 'rgb(255, 0, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)'];
-      for (var i = 0; i < numberToFill; i++) {
-        context.fillStyle = fillChoices[Math.floor(fillChoices.length * Math.random())];
 
-        var rx = Math.floor(Math.random() * Math.floor(maxX / distanceBetweenVerticalLines)) * distanceBetweenVerticalLines;
-        var ry = Math.floor(Math.random() * Math.floor(maxY / distanceBetweenHorizontalLines)) * distanceBetweenHorizontalLines;
-        var length = distanceBetweenVerticalLines * Math.floor(Math.random() * 3);
-        context.fillRect(rx, ry, length, length);
+      var filledSquares = [];
+      for (var i = 0; i < numberToFill; i++) {
+        filledSquares.push({
+          x: Math.floor(Math.random() * Math.floor(maxX / distanceBetweenVerticalLines)) * distanceBetweenVerticalLines,
+          y: Math.floor(Math.random() * Math.floor(maxY / distanceBetweenHorizontalLines)) * distanceBetweenHorizontalLines,
+          length: distanceBetweenVerticalLines * Math.floor(Math.random() * 3),
+          fillColor: fillChoices[Math.floor(fillChoices.length * Math.random())]
+        });
+      }
+
+      var alpha = 0;
+      var growthState = 'up';
+      var stallCount = 0;
+      squareAnimationInterval = setInterval(function() {
+        switch (growthState) {
+          case 'up':
+            alpha += 0.025;
+            if (alpha >= 1) {
+              alpha = 1;
+              growthState = 'stallUp';
+            }
+            break;
+
+          case 'down':
+            alpha -= 0.025;
+            if (alpha <= 0) {
+              alpha = 0;
+              growthState = 'stallDown';
+            }
+            break;
+
+          case 'stallUp':
+          case 'stallDown':
+            stallCount += 1;
+            if (stallCount > 70) {
+              stallCount = 0;
+              growthState = growthState === 'stallUp' ? 'down' : 'up';
+            }
+            break;
+        }
+
+        drawFilledSquares(filledSquares, alpha);
+      }, 60);
+    }
+
+    function drawFilledSquares(filledSquares, alpha) {
+      if (alpha === undefined) alpha = 1;
+
+      for (var i = 0; i < filledSquares.length; i++) {
+        var filledSquare = filledSquares[i];
+        context.clearRect(filledSquare.x, filledSquare.y, filledSquare.length, filledSquare.length);
+      }
+
+      context.globalAlpha = 1.0;
+      drawGrid();
+
+      context.globalAlpha = alpha;
+
+      for (i = 0; i < filledSquares.length; i++) {
+        var filledSquare = filledSquares[i];
+        context.fillStyle = filledSquare.fillColor;
+        context.fillRect(filledSquare.x, filledSquare.y, filledSquare.length, filledSquare.length);
       }
     }
   }
